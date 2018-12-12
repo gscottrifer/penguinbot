60f7623d3865daa6f3abd644bf4b1c59bbc7b1e1
Rcherry88 <44907464+Rcherry88@users.noreply.github.com>
2018-12-12 16:20:21 -0500

    Create Final Sensors.h

---
+++ Final Sensors.h
@@ -0,0 +1,17 @@
+// Created on Wed March 21 2018
+
+// Replace FILE with your file's name
+#ifndef _FILE_H_
+#define _FILE_H_
+
+float left_photo_value;
+float right_photo_value;
+int photo_offset      = 80;
+float photo_coeff     = 5.0;
+float photo_difference;
+float photo_ratio;
+float photo_threshold = 40.0;	
+float photo_max       = 1023.0;
+int avoid_threshold   = 600;
+
+#endif
